6d1dbc2e0410767edd2e52aaa8a39d4787c5fb04
Zayar Aung <z.zayar99@gmail.com>
2024-10-15 23:37:40 +0900

    Gun, impact effects

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float HP = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float dmg){
+        HP -= dmg;
+        if(HP <= 0f){
+            Death();
+        }
+    }
+
+    public void Death(){
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapons/GunScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float damage = 10f;
+    public float fireRate = 15f;
+    public float range = 100f;
+    private float nextFire = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= nextFire){
+            nextFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
+    }
+
+    public void Shoot(){
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null){
+                enemy.TakeDamage(damage);
+            }
+
+            GameObject ie = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(ie, 2f);
+        }
+    }
+}
