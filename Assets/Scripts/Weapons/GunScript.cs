563c5bbd929b1c5915d04fac31f4ff06778e0f4e
Zayar Aung <z.zayar99@gmail.com>
2024-10-18 21:54:27 +0900

    Shotgun spread, gun mark removal, WIP ADS

--- Assets/Scripts/Weapons/GunScript.cs
+++ Assets/Scripts/Weapons/GunScript.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GunScript : MonoBehaviour
 {
@@ -16,15 +17,18 @@ public class GunScript : MonoBehaviour
     public GameObject ReloadText;
     public string gunName = "Gun";
     public float damage = 10f;
-    public float fireRate = 15f;
+    public float fireRate = 0.1f;
     public float range = 100f;
     public int maxAmmo = 30;
-    public int currentAmmo;
+    private int currentAmmo;
     public float reloadTime = 2f;
     private bool isReloading = false;
     private float nextFire = 0f;
     public bool singleShot = false;
     public bool isShotGun = false;
+    private float defaultZoom = 60f;
+    public float ADSZoom = 40f;
+    public float ADSSpeed = 120f;
 
     void Awake(){
         currentAmmo = maxAmmo;
@@ -49,17 +53,24 @@ public class GunScript : MonoBehaviour
         if(isReloading)
             return;
 
+        // ADS
+        if(Input.GetButton("Fire2")){
+            cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, ADSZoom, ADSSpeed * Time.deltaTime);
+        }
+        else if(cam.fieldOfView < defaultZoom){
+            cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, defaultZoom, ADSSpeed * Time.deltaTime);
+        }
         // Shoot
         if(currentAmmo > 0){
             if(singleShot && Input.GetButtonDown("Fire1") && Time.time >= nextFire){
-                nextFire = Time.time + 1f/fireRate;
+                nextFire = Time.time + fireRate;
                 if(isShotGun)
                     ShotgunShoot();
                 else
                     Shoot();
             }
             else if(!singleShot && Input.GetButton("Fire1") && Time.time >= nextFire){
-                nextFire = Time.time + 1f/fireRate;
+                nextFire = Time.time + fireRate;
                 Shoot();
             }
         }
@@ -81,17 +92,21 @@ public class GunScript : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range)){
             Debug.Log(hit.transform.name);
-            Debug.Log(cam.transform.forward);
+            //Debug.Log(cam.transform.forward);
+
+            GameObject ie = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(ie, 2f);
 
             Enemy enemy = hit.transform.GetComponent<Enemy>();
             if(enemy != null){
                 enemy.TakeDamage(damage);
+                ie.transform.parent = enemy.transform;
             }
 
-            GameObject ie = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(ie, 2f);
+            //cam.transform.Rotate(5f, 0, 0);
         }
     }
+
 
     public void ShotgunShoot(){
         muzzleFlash.Play();
@@ -99,20 +114,24 @@ public class GunScript : MonoBehaviour
         currentAmmo = Math.Max(0, currentAmmo - 1);
         UpdateAmmoText();
 
-        Vector3[] shotgunDir = {cam.transform.forward + cam.transform.right * 0.2f, cam.transform.forward + cam.transform.right * -0.2f, 
-            cam.transform.forward + cam.transform.up * 0.2f, cam.transform.forward + cam.transform.up * -0.2f};
-        for(int i = 0; i < 4; i++){
+        // Vector3[] shotgunDir = {cam.transform.forward + cam.transform.right * 0.2f, cam.transform.forward + cam.transform.right * -0.2f, 
+        //     cam.transform.forward + cam.transform.up * 0.2f, cam.transform.forward + cam.transform.up * -0.2f};
+        Vector3[] shotgunDir = {cam.transform.forward + cam.transform.up * Random.Range(0.01f, 0.2f), cam.transform.forward + cam.transform.right * Random.Range(0.01f, 0.2f) + cam.transform.up * Random.Range(0.01f, 0.2f), 
+            cam.transform.forward + cam.transform.right * Random.Range(0.01f, 0.2f) + cam.transform.up * Random.Range(-0.2f, -0.01f), cam.transform.forward + cam.transform.up * Random.Range(-0.2f, -0.01f),
+            cam.transform.forward + cam.transform.right * Random.Range(-0.2f, -0.01f) + cam.transform.up * Random.Range(-0.2f, -0.01f), cam.transform.forward + cam.transform.right * Random.Range(-0.2f, -0.01f) + cam.transform.up * Random.Range(0.01f, 0.2f)};
+        for(int i = 0; i < 6; i++){
             RaycastHit hit;
             if(Physics.Raycast(cam.transform.position, shotgunDir[i].normalized, out hit, range)){
-                Debug.Log(cam.transform.forward + shotgunDir[i]);
+                //Debug.Log(cam.transform.forward + shotgunDir[i]);
+
+                GameObject ie = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(ie, 2f);
 
                 Enemy enemy = hit.transform.GetComponent<Enemy>();
                 if(enemy != null){
                     enemy.TakeDamage(damage);
+                    ie.transform.parent = enemy.transform;
                 }
-
-                GameObject ie = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                Destroy(ie, 2f);
             }
         }
     }
