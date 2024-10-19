6a7ea09c5258ae1f76700031f8a6897ed1c29822
Kiliken <francescopaolomariani7@gmail.com>
2024-10-19 18:38:31 +0900

    Melee Enemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] float attackRange;
+    NavMeshAgent enemyAI;
+    Transform playerPos;
+
     public float HP = 50f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAI = this.GetComponent<NavMeshAgent>();
+        if (!(playerPos = GameObject.Find("Player").GetComponent<Transform>()))
+            Debug.LogError("NO OBJECT PLAYER FOUND");
     }
 
     // Update is called once per frame
@@ -17,6 +25,18 @@ public class Enemy : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        enemyAI.SetDestination(GetRadius(transform.position,playerPos.position,attackRange));
+    }
+
+    Vector3 GetRadius(Vector3 enemyPos, Vector3 playerPos, float radius)
+    {
+        Vector3 direction = (enemyPos - playerPos).normalized;
+        return playerPos + direction * radius;
+    }
+    
 
     public void TakeDamage(float dmg){
         HP -= dmg;
---
+++ Assets/Scripts/Enemies/EnemyMelee.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyMelee : MonoBehaviour
+{
+    [Header("Melee")]
+    [SerializeField]float radius;
+
+    float timer;
+    bool inRange;
+
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        if (inRange)
+        {
+            timer += 1 * Time.deltaTime;
+            if(timer > 1f)
+            {
+                Debug.Log("GotHitten");
+                timer = 0f;
+            }
+        } else
+        {
+            timer = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("in");
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("out");
+            inRange = false;
+        }
+    }
+
+}
